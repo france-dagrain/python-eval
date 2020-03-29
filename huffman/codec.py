0691d1c3071eb5f65ef9497bd080d3504aeb2d18
france-dagrain <francedagrain@gmail.com>
2020-03-29 21:13:30 +0200

    on a les premiers résultats mais décalage des deux

---
+++ huffman/codec.py
@@ -0,0 +1,4 @@
+from codec import TreeBuilder, Codec
+
+text = "a dead dad ceded a bad babe a beaded abaca bed"
+
--- needleman_wunsch/ruler.py.py
+++ needleman_wunsch/ruler.py.py
@@ -1,68 +1,75 @@
-#Ecrire l'algorithme NW avec la matrice  et les scores Match=0, Mismatch=1, Gap=1 --> il faut minimiser la distance 
-
-#os.makedirs(needleman_wunsch)
-
-#from needleman_wunsch import Ruler
 
 import numpy as np
 
-class Ruler: 
-
-    def match(self,m,n) :
+def match(m,n) :
         if m==n :
             return 0
         else : 
             return 1
- 
+
+class Ruler: 
+
+    def __init__ (self,a,b) : 
+        self.a = a
+        self.b = b
+              
 
 #construction de la matrice
     def compute(self,a,b) :
+        #transformation en listes
         a = [i for i in a]
         b= [i for i in b] 
-        x = len(a) +1
-        y = len(b) +1
+        x = len(a) + 1
+        y = len(b) + 1
         #initialison la matrice à zéro
-        mat1 = np.zeros(y,x)
-        #remplir la prmière colonne et la première ligne des pénalités du gap, soit 1 multuplié par leur éloignement de l'origine
-        for i in range (0,x) : 
-            mat1[i][0] = i
+        mat = np.zeros((y,x))
+        #remplir la première colonne et la première ligne des pénalités du gap, soit 1 multuplié par leur éloignement de l'origine
         for i in range (0,y) : 
-            mat1[0][i]=  i
+            mat[i][0] = i
+        for i in range (0,x) : 
+            mat[0][i]=  i
         #remplir le reste de la matrice en trouvant le chemin qui minimise la distance en partant de 0 en calculant le score à chaque étape
-        mat1[0][0]=0
+        mat[0][0]=0
         for i in range (1,y) :
             for j in range (1,x): 
-                if a[j-1] == b[i-1] : #lors d'un match
-                    dia = mat1[i-1][j-1] 
-                else :
-                    dia =mat1[i-1][j-1] +1 #pénalité du mismatch
-                #déplacement vertical ou horizontal entraine un gap et donc une pénalité de 1
-                hor = mat1[i-1][j] + 1 
-                ver = mat1[i][j-1] + 1
-                mat1[i][j] = min(dia, hor, ver) #on essaie de minimiser la distance 
-        return mat1
+                    dia = mat[j-1][i-1] + match(a[j-1],b[i-1])
+                    #déplacement vertical ou horizontal entraine un gap et donc une pénalité de 1
+                    hor = mat[i-1][j] + 1 
+                    ver = mat[i][j-1] + 1
+                    mat[i][j] = min(dia, hor, ver) #on essaie de minimiser la distance 
+        return mat
+        print(mat)
 
 #calculons maintenant la distance en partant de la fin, en remontant la matrice 
-    def distance(self,a,b) :
-        i=a
-        j=b
+    def distance(self,a,b,mat) :
+        #initialisation au coin au bas à gauche 
+        i=len(a)
+        j=len(b)
+        global top, bottom, s1, s2
+        top =''
+        s1 =''
+        bottom =''
+        s2 = ''
+
         while i > 0 and j> 0 : 
-            note_act = mat1[i][j]
-            note_dia = mat1[i-1][j-1]
-            note_ver = mat1[i-1][j]
-            note_hor = mat1[i][j-1]
-
-
+            note_act = mat[j][i]
+            note_dia = mat[j-1][i-1]
+            note_ver = mat[j-1][i]
+            note_hor = mat[j][i-1]
+            
             if note_act == note_dia + match(a[i-1], b[j-1]) : #on peut remonter par la diagonale 
-                s1,s2 = a[i-1],b[j-1]
+                s1 = a[i-1]
+                s2 = b[j-1]
                 i,j = i-1,j-1
                 
             elif note_act == note_ver + 1:
-                s1,s2 = a[i-1],'-'
+                s1 = str(a[i-1])
+                s2 = '-'
                 i -= 1
 
             elif note_act == note_hor + 1:
-                s1,s2 = '-',b[j-1]
+                s1 = '-'
+                s2 = str(b[j-1])
                 j -= 1
             
             top += s1
@@ -70,32 +77,38 @@ class Ruler:
 
             #Attention , si jamais j== 0 avant i, on continue à bouger de manière verticale (même chose sur j ensuite)
         while i > 0:
-            s1,s2 = a[i-1],'-'
+            s1,s2 = a[i-1],'='
             top += s1
             bottom += s2
             i -= 1
         while j > 0:
-            s1,s2 = '-',b[j-1]
+            s1,s2 = '=',b[j-1]
             top += s1
             bottom += s2
             j -= 1
     
         top = top[::-1]
         bottom = bottom[::-1]
-        len(top)
-        distance = 0
+        print(top)
+        print(bottom)
+        
+        dist = 0
         for i in range(len(top)):
             s1 = top[i]
             s2 = bottom[i]
             if not s1 == s2:
-                distance += 1
+                dist += 1
     
-        return distance
+        return dist
+        print(dist)
         return top
+        print(top)
         return bottom
+        print(bottom)
 
-ruler = Ruler("abcdefghi","abcdfghi")
-
+ruler = Ruler("TCGCA","TCCA")
+print(ruler.compute("TCGCA","TCCA"))
+print(ruler.distance("TCGCA","TCCA",ruler.compute("TCGCA","TCCA")))
 
 
 
